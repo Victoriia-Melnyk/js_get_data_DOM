ff0c6511848d71368595537df012b64909620465
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-07-09 10:22:39 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+let totalPopulation = 0;
+let countriesAmount = 0;
+const populations = [
+    ...document.querySelectorAll(".population")
+].map((population)=>{
+    return population.textContent;
+});
+populations.forEach((populationString)=>{
+    const number = Number(populationString.split(",").join(""));
+    totalPopulation += number;
+    countriesAmount += 1;
+});
+const averagePopulation = totalPopulation / countriesAmount;
+const totalSpan = document.querySelector(".total-population");
+const averageSpan = document.querySelector(".average-population");
+totalSpan.textContent = totalPopulation.toLocaleString("en-US");
+averageSpan.textContent = averagePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
